350752db2f8a1a7d32fd4cb31bc742c96992b353
Jonathan Fernandez <128869367+jonathandev31@users.noreply.github.com>
2023-04-14 10:52:29 -0500

    02_Ejercicio

---
+++ 02_Ejercicio.js
@@ -0,0 +1,10 @@
+const PI = 3.1416
+console.log (`El valor dew PI: ${PI}`)
+
+// Punto a
+
+const segundosEnMinuto = 60;
+const minutosEnHora = 60;
+const horasEnDia = 24;
+
+console.log (`Un minuto tiene ${segundosEnMinuto} segundos; una hora tiene ${minutosEnHora} minutos; una hora tiene ${horasEnDia} horas.`)
