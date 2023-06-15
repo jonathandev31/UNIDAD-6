dee86b8187d7cb1d5186bf2097e7322442ebbb8c
Jonathan Fernandez <128869367+jonathandev31@users.noreply.github.com>
2023-06-14 23:07:26 -0500

    Cambios en el archivo manejoErrores y 15_Ejercicio

--- 15_Ejercicio.js
+++ 15_Ejercicio.js
@@ -52,7 +52,7 @@ try {
     let numero= 4;
     if(isNaN(numero)){ //El metodo isNaN() evalua si la variable es de tipo numererico o no
         //Definición del nuevo mensaje de error  throw new Error()
-//console.log(isNaN(numero))
+        //console.log(isNaN(numero))
         throw new Error("El caracter introducido no es un numero")
     }
     console.log(numero*numero)
@@ -67,11 +67,58 @@ try {
 try {
     let numero1 = "7";
     if (isNaN(numero1)) throw new Error ("¡Cuidado! No se reciben datos no numéricos.");
-    console.log ("La variable " + num + " si es de un numero")
+    console.log ("La variable " + numero1 + " si es de un numero")
 } catch (error) {
     console.log(error);
 } finally {
     console.log("Análisis de datos terminado.")
 }
 
+// Punto 5
 
+    function errorTipoNumerico (numeroUno, nuemroDos) {
+        if ( isNaN (numeroUno) || isNaN (nuemroDos) ) throw new Error ("No se reciben datos no numéricos.");
+    }
+
+    function suma (numeroUno, nuemroDos){
+        errorTipoNumerico (numeroUno, nuemroDos);
+        return numeroUno + nuemroDos;
+
+    }
+    function resta (numeroUno, nuemroDos){
+        errorTipoNumerico (numeroUno, nuemroDos);
+        return numeroUno - nuemroDos;
+    }
+
+    function division (numeroUno, nuemroDos){
+        errorTipoNumerico (numeroUno, nuemroDos);
+        if ( nuemroDos == 0 ) throw new Error ("La division por cero no es posible."); 
+        return numeroUno / nuemroDos;
+    }
+
+    function multiplicacion (numeroUno, nuemroDos){
+        errorTipoNumerico (numeroUno, nuemroDos);
+        return numeroUno * nuemroDos;
+    }
+
+    function potencia (numeroUno, nuemroDos) {
+        errorTipoNumerico (numeroUno, nuemroDos);
+        return numeroUno ** nuemroDos;
+    }
+
+    function raizCuadrada (numeroUno) {
+        errorTipoNumerico (numeroUno, 0);
+        if ( numeroUno < 0 ) throw new Error ("No es posible hacer la raiz cuadrada a numero negativos"); 
+        return Math.sqrt( numeroUno )
+    }
+
+try {
+    console.log (resta (4,"9"));
+    console.log (suma (4,7));
+    console.log (division (4,7));
+    console.log (multiplicacion (4,7))
+    console.log (potencia (4,7));
+    console.log (raizCuadrada (-4));
+} catch (error) {
+    console.log (error);
+}
--- INICIOS JS/manejoErrores.js
+++ INICIOS JS/manejoErrores.js
@@ -13,3 +13,14 @@ try {
 } catch (error) {
     console.log("Hay un error! " + error); 
 }
+
+try {
+    let numeroUsuario = 9;
+    if ( isNaN ( numeroUsuario ) || numeroUsuario < 1 || numeroUsuario > 9 ) {
+        throw new Error ( "Numero introducido no valido" );
+    } else {
+        console.log( "Sin problemas" );
+    }
+} catch ( error ) {
+    console.log( " Se ha producido un error! " + error );
+}
