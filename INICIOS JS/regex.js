1e6d4ee6a8681e1545a2e3cf446fb21e7a369239
Jonathan Fernandez <128869367+jonathandev31@users.noreply.github.com>
2023-06-15 20:28:33 -0500

    regex, 16_Ejercicio, 17_Ejercicio

--- INICIOS JS/regex.js
+++ INICIOS JS/regex.js
@@ -1,11 +1,29 @@
 // Se hara un ejemplo de una expresion regular
 
 let miRegEx = /as?.a/;
-let msg = 'as?.a';
+let msg = ' as?.a';
 
-let cadenas = [ 'casamentero', 'castaño', 'sabina', 'casualidad' ];
+let cadenas = [ 'casamentero', 'castaño', 'sabina', 'casualidad', 'asador', 'canasta', 'java' ];
 
 for (i of cadenas) {
     sal = "¿Contiene " + i + " el patron" + msg + " : " + miRegEx.test(i);
     console.log(sal);
+}
+
+// Ejercicio de clase 
+
+let miRegExDos = /^[A-C]\w+\ses\s\w+/;
+let msgDos = ' ^[A-C]\w+\ses\w+';
+
+let cadenasDos = [ 'Juan es guapo', 
+                'Adriano no es feo', 
+                'Adriano deja de ser guapo', 
+                'Adriano ya es guapo', 
+                'No es ahora', 
+                'Ahora es no', 
+                'Adriano es guapo' ];
+
+for ( i of cadenasDos ) {
+    salDos = "¿Contiene " + i + " el patron" + msgDos + "? : " + miRegExDos.test( i );
+    console.log(salDos);
 }
